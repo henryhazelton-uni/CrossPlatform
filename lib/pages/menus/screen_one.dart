af74ef19739e788feb19d69ea15c3e8682c13d45
henryhazelton-uni <30250725@blackpool.ac.uk>
2026-01-30 09:13:14 +0000

    added imports

--- lib/pages/menus/screen_one.dart
+++ lib/pages/menus/screen_one.dart
@@ -1,9 +1,9 @@
-// Menu Screen
+// Screen One
 
 import 'package:flutter/material.dart';
 
 class MenuScreen extends StatelessWidget {
-  const MenuScreen({Key? key, required String title}) : super(key: key);
+  const MenuScreen({super.key, required String title});
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -12,35 +12,22 @@ class MenuScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text(
-              'Welcome to Multi-Screen App',
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
+            const Text('Welcome to Multi-Screen App', style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
             const SizedBox(height: 40),
             ElevatedButton(
               onPressed: () {
                 // TODO: Navigate to Screen One
               },
+              style: ElevatedButton.styleFrom(padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 15)),
               child: const Text('Go to Screen One'),
-              style: ElevatedButton.styleFrom(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 40,
-                  vertical: 15,
-                ),
-              ),
             ),
             const SizedBox(height: 20),
             ElevatedButton(
               onPressed: () {
                 // TODO: Navigate to Screen Two
               },
+              style: ElevatedButton.styleFrom(padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 15)),
               child: const Text('Go to Screen Two'),
-              style: ElevatedButton.styleFrom(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 40,
-                  vertical: 15,
-                ),
-              ),
             ),
           ],
         ),
